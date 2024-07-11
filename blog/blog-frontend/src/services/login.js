2c0f37b60d3d129b4afc11e5694daee7666ee3ac
Muhammad Huzaifa <mhuzaifa3737@gmail.com>
2024-07-12 03:40:30 +0500

    Delete blog directory

--- blog/blog-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const loginUrl = '/api/login';
-
-const login = async (credentials) => {
-  const login = await axios.post(loginUrl, credentials);
-  return login.data;
-};
-
-export default { login };
